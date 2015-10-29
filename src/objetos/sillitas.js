f04a8bb5756a992bd745abfe0aef8386cb619bd7
BarAgCud <aguila.cudicio@gmail.com>
2015-10-28 22:31:55 -0300

    Casi estan terminadas las sillitas

---
+++ src/objetos/sillitas.js
@@ -0,0 +1,29 @@
+
+function Sillitas()
+{
+    Objeto.call(this, null, null);
+    
+    var base = new BaseSillitas();   
+     
+    var techo = new TechoSillitasCompleto();
+    mat4.translate(techo.matrices, techo.matrices, [0,6,0]);
+    //mat4.rotate(techo.matrices, techo.matrices, Math.PI/12, [1,0,0]);
+
+
+    this.hijos.push(base);
+    this.hijos.push(techo);
+}
+
+heredarPrototype(Sillitas, Objeto);
+/**
+ * Rotacion
+ */
+Sillitas.prototype.update = function(deltaT) 
+{
+    //Velocidad de rotacion del techo
+    var velocidadAngular=2;
+
+   mat4.rotate(this.hijos[1].matrices, this.hijos[1].matrices, velocidadAngular*deltaT, [0.0,1.0,0.0]);
+ 
+    Objeto.prototype.update.call(this,deltaT);
+};
--- src/objetos/techoSillitas.js
+++ src/objetos/techoSillitas.js
@@ -1,6 +1,6 @@
 var TechoSillitas = function() 
 {  
-    vertices=[0,0,0.8,0,4,0.5,4,1.4,0,1.4];
+    vertices=[0,0,0.8,0,5,0.5,5,1.4,0,1.4];
 
     SuperficieRevolucion.call(this,"texturas/debug.jpg",vertices,200);
     
@@ -8,3 +8,62 @@ var TechoSillitas = function()
 
 heredarPrototype(TechoSillitas,SuperficieRevolucion); 
  
+var TechoSillitasCompleto = function() 
+{  
+    Objeto.call(this, null, null);
+    
+    var techo = new TechoSillitas();
+
+    var anguloRot=Math.PI/4;
+    
+    //Cuantas sillitas voy a tener
+    var numSillitas=5;
+    
+    this.hijos.push(techo);
+
+    for (var j=1; j<=numSillitas; j++) {
+      
+      var angulo=(2*(Math.PI)/(parseFloat(numSillitas)))*j;
+      
+      var posicion=[Math.cos(angulo),Math.sin(angulo)];
+     
+      var soga = new SillitasSoga();
+      
+      mat4.translate(soga.matrices, soga.matrices, [5*posicion[0],-1,5*posicion[1]]);
+      mat4.rotate(soga.matrices, soga.matrices, Math.PI/2, [1,0,0]);
+      
+      mat4.rotate(soga.matrices, soga.matrices, anguloRot, [-posicion[1],posicion[0],0]);
+      
+      this.hijos.push(soga);
+    }
+ 
+};
+
+heredarPrototype(TechoSillitasCompleto,Objeto); 
+ 
+
+
+var SillitasSoga = function()
+{
+  Objeto.call(this, null, null);
+  //soga ""larga""
+  var sogap = new Cilindro(12, 0, "texturas/pixel.png" );
+  mat4.scale(sogap.matrices, sogap.matrices, [0.1,0.1,2]); 
+  
+  var soga2 = new Cilindro(12, 0, "texturas/pixel.png" );
+  mat4.translate(soga2.matrices, soga2.matrices, [-0.35,0,2.25]);
+  mat4.rotate(soga2.matrices, soga2.matrices, -Math.PI/4, [0,1,0]);
+  mat4.scale(soga2.matrices, soga2.matrices, [0.1,0.1,0.5]); 
+  
+  var soga3 = new Cilindro(12, 0, "texturas/pixel.png" );
+  mat4.translate(soga3.matrices, soga3.matrices, [0.35,0,2.25]);
+  mat4.rotate(soga3.matrices, soga3.matrices, Math.PI/4, [0,1,0]);
+  mat4.scale(soga3.matrices, soga3.matrices, [0.1,0.1,0.5]); 
+  
+  this.hijos.push(sogap);
+  this.hijos.push(soga2);
+  this.hijos.push(soga3);
+};
+
+
+heredarPrototype(SillitasSoga,Objeto); 
